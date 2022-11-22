47eb31bf42cd4b9b0c0668093cad646efd14174f
aburakt <info@aburakt.com>
2022-11-22 22:22:16 +0300

    bootstrap added

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-const map = L.map('map').setView([39.92, 32.854], 5)
+// map settings
+const map = L.map('map').setView([38.2, 34.6], 0)
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 14,
@@ -7,32 +8,27 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
-var lat = Math.floor(Math.random() * (4100 - 3700) + 3700) / 100
-var lon = Math.floor(Math.random() * (4300 - 2700) + 2700) / 100
-console.log('Boylam:', lat, 'Enlem:', lon)
+// ramdom markers
+function RandomMarker(number) {
+  for (let i = 0; i < number; i++) {
+    const lat = Math.floor(Math.random() * (4100 - 3700) + 3700) / 100
+    const lon = Math.floor(Math.random() * (4300 - 2700) + 2700) / 100
+    console.log('Boylam:', lat, '| Enlem:', lon)
 
-var lat2 = Math.floor(Math.random() * (4100 - 3700) + 3700) / 100
-var lon2 = Math.floor(Math.random() * (4300 - 2700) + 2700) / 100
-console.log('Boylam:', lat2, 'Enlem:', lon2)
+    const marker = L.marker([lat, lon])
+      .addTo(map)
+      .bindPopup(`<b>Merhaba, burası</b><br />Boylam: ${lat} | Enlem: ${lon}`)
+  }
+}
+RandomMarker(25)
 
-const marker = L.marker([lat, lon])
-  .addTo(map)
-  .bindPopup('<b>Hello world!</b><br />I am a popup.')
-// .openPopup()
-
-L.marker([lat2, lon2])
-  .addTo(map)
-  .bindPopup('<b>Hello world!</b><br />I am a popup.')
-
-const popup = L.popup()
-  .setLatLng([39.935, 32.854])
-  .setContent('I am a standalone popup.')
-// .openOn(map)
+// popup settings
+const popup = L.popup().setLatLng([39.935, 32.854]).setContent('Popup')
 
 function onMapClick(e) {
   popup
     .setLatLng(e.latlng)
-    .setContent(`You clicked the map at ${e.latlng.toString()}`)
+    .setContent(`Seçtiğin nokta: ${e.latlng.toString()}`)
     .openOn(map)
 }
 
